674551c34c0bdc1b63afce18bdc0f1f4a5bb9d0e
Albert Hernandez <alberthernandezdev@gmail.com>
2024-08-17 21:00:03 +0200

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 import type { Metadata } from "next";
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
