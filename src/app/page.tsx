ac80baf777d53766f51d7d78725ba9f7c17ab355
Albert Hernandez <alberthernandezdev@gmail.com>
2024-08-24 15:09:28 +0200

    v12
    Signed-off-by: Albert Hernandez <alberthernandezdev@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,11 @@ import {
   Code,
   ExternalLink,
   FileText,
-  Github,
   Lightbulb,
-  Linkedin,
   Mail,
   Moon,
   Star,
   Sun,
-  Twitter,
   Users,
   Youtube,
 } from "lucide-react";
@@ -234,28 +231,68 @@ export default function Portfolio() {
                   aria-label="GitHub"
                   className="text-muted-foreground hover:text-primary"
                 >
-                  <Github className="h-6 w-6" />
+                  <svg
+                    role="img"
+                    class="fill-current"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                  >
+                    <title>GitHub</title>
+                    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                  </svg>
                 </Link>
                 <Link
                   href="https://linkedin.com/in/alberthernandez"
                   aria-label="LinkedIn"
                   className="text-muted-foreground hover:text-primary"
                 >
-                  <Linkedin className="h-6 w-6" />
+                  <svg
+                    role="img"
+                    class="fill-current"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                  >
+                    <title>LinkedIn</title>
+                    <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                  </svg>
                 </Link>
                 <Link
                   href="https://twitter.com/alberthernandez"
                   aria-label="Twitter"
                   className="text-muted-foreground hover:text-primary"
                 >
-                  <Twitter className="h-6 w-6" />
+                  <svg
+                    role="img"
+                    class="fill-current"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                  >
+                    <title>X</title>
+                    <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+                  </svg>
                 </Link>
                 <Link
                   href="https://youtube.com/alberthernandez"
                   aria-label="YouTube"
                   className="text-muted-foreground hover:text-primary"
                 >
-                  <Youtube className="h-6 w-6" />
+                  <svg
+                    role="img"
+                    class="fill-current"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={24}
+                    height={24}
+                  >
+                    <title>YouTube</title>
+                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                  </svg>
                 </Link>
               </div>
             </div>
@@ -309,7 +346,17 @@ export default function Portfolio() {
                 </div>
               </div>
               <div className="flex items-start space-x-4">
-                <Youtube className="w-8 h-8 text-primary dark:text-primary-dark flex-shrink-0 mt-1" />
+                <svg
+                  className="w-8 h-8 text-primary dark:text-primary-dark flex-shrink-0 mt-1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  role="img"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <title>YouTube</title>
+                  <path d="M23.498 6.186a2.963 2.963 0 0 0-2.084-2.084C19.963 3.5 12 3.5 12 3.5s-7.963 0-9.414.602A2.963 2.963 0 0 0 .502 6.186C0 7.638 0 12 0 12s0 4.362.502 5.814a2.963 2.963 0 0 0 2.084 2.084C4.037 20.5 12 20.5 12 20.5s7.963 0 9.414-.602a2.963 2.963 0 0 0 2.084-2.084C24 16.362 24 12 24 12s0-4.362-.502-5.814zM9.546 15.568V8.432L15.818 12l-6.272 3.568z" />
+                </svg>
+
                 <div>
                   <h3 className="text-xl font-bold mb-2">Content Creator</h3>
                   <p className="text-gray-600 dark:text-gray-300">
@@ -455,7 +502,17 @@ export default function Portfolio() {
                     rel="noopener noreferrer"
                     className="flex items-center text-sm text-primary hover:underline"
                   >
-                    <Github className="w-4 h-4 mr-1" />
+                    <svg
+                      role="img"
+                      className="fill-current"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={24}
+                      height={24}
+                    >
+                      <title>GitHub</title>
+                      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                    </svg>
                     View on GitHub
                     <ExternalLink className="w-3 h-3 ml-1" />
                   </a>
@@ -484,28 +541,68 @@ export default function Portfolio() {
               aria-label="GitHub"
               className="text-muted-foreground hover:text-primary"
             >
-              <Github className="h-5 w-5" />
+              <svg
+                role="img"
+                className="fill-current"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+              >
+                <title>GitHub</title>
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+              </svg>
             </Link>
             <Link
               href="https://linkedin.com/in/alberthernandez"
               aria-label="LinkedIn"
               className="text-muted-foreground hover:text-primary"
             >
-              <Linkedin className="h-5 w-5" />
+              <svg
+                role="img"
+                className="fill-current"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+              >
+                <title>LinkedIn</title>
+                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+              </svg>
             </Link>
             <Link
               href="https://twitter.com/alberthernandez"
               aria-label="Twitter"
               className="text-muted-foreground hover:text-primary"
             >
-              <Twitter className="h-5 w-5" />
+              <svg
+                role="img"
+                className="fill-current"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+              >
+                <title>X</title>
+                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+              </svg>
             </Link>
             <Link
               href="https://youtube.com/alberthernandez"
               aria-label="YouTube"
               className="text-muted-foreground hover:text-primary"
             >
-              <Youtube className="h-5 w-5" />
+              <svg
+                role="img"
+                className="fill-current"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+              >
+                <title>YouTube</title>
+                <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+              </svg>
             </Link>
           </div>
         </div>
