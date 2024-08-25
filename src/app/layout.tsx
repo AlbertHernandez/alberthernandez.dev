e7ad8a857c32c4e5891a01569f88cd6cc6f99e3f
Albert Hernandez <alberthernandezdev@gmail.com>
2024-08-25 17:43:11 +0200

    v70
    Signed-off-by: Albert Hernandez <alberthernandezdev@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 import type { Metadata } from "next";
 
@@ -72,7 +73,13 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: [{ url: "/favicon.ico" }],
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+    other: [{ rel: "manifest", url: "/site.webmanifest" }],
   },
   alternates: {
     canonical: "https://alberthernandez.dev",
