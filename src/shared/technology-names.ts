f82c590576a4bccdda5248f633b71414a1a75de2
Albert Hernandez <alberthernandezdev@gmail.com>
2024-08-30 21:07:11 +0200

    refactor: add more technology names

--- src/shared/technology-names.ts
+++ src/shared/technology-names.ts
@@ -3,6 +3,18 @@ export const TechnologyNames = {
   JavaScript: "JavaScript",
   React: "React",
   TypeScript: "TypeScript",
+  Jest: "Jest",
+  MongoDB: "MongoDB",
+  Redis: "Redis",
+  Docker: "Docker",
+  GitHubActions: "GitHub Actions",
+  AmazonWebServices: "Amazon Web Services",
+  Datadog: "Datadog",
+  NestJS: "NestJS",
+  PostgreSQL: "PostgreSQL",
+  Kubernetes: "Kubernetes",
+  CircleCI: "CircleCI",
+  GoogleCloudPlatform: "Google Cloud Platform",
 } as const;
 
 export type TechnologyNameKey = keyof typeof TechnologyNames;
