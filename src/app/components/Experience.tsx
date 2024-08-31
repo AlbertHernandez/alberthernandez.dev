a60d10a1b97f396ed278761c8e2e4dd12a4fd76f
Albert Hernandez <alberthernandezdev@gmail.com>
2024-08-31 15:53:46 +0200

    refactor: create experience card

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 import React from "react";
 
-import { TechnologyTag } from "@/app/components/TechnologyTag";
+import { ExperienceCard } from "@/app/components/ExperienceCard";
 import { WorkExperience } from "@/app/data/types";
-import { ExternalLink } from "@/shared/icons";
-import { formatDateRange } from "@/shared/utils/dateFormatter";
 
 interface ExperienceProps {
   experiences: WorkExperience[];
@@ -25,70 +21,11 @@ export const Experience: React.FC<ExperienceProps> = ({ experiences }) => {
         </h2>
         <div className="space-y-16">
           {experiences.map((experience, index) => (
-            <div
+            <ExperienceCard
               key={experience.company.name}
-              className="relative flex items-start"
-            >
-              {index !== experiences.length - 1 && (
-                <div className="absolute top-0 left-6 h-[calc(100%+4rem)] w-px bg-gray-300 dark:bg-gray-700" />
-              )}
-              <div className="flex-shrink-0 w-12 h-12 rounded-full border-4 border-white dark:border-gray-800 bg-white dark:bg-gray-800 z-10">
-                <Image
-                  src={experience.company.logo}
-                  alt={`${experience.company.name} logo`}
-                  width={48}
-                  height={48}
-                  className="rounded-full"
-                />
-              </div>
-              <div className="ml-6 flex-grow">
-                <div className="flex flex-col sm:flex-row justify-between items-start mb-2">
-                  <div>
-                    <a
-                      href={experience.company.website}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="inline-flex items-center text-xl font-bold text-primary hover:underline mb-1 group"
-                    >
-                      {experience.company.name}
-                      <ExternalLink className="ml-1 h-4 w-4 opacity-0 group-hover:opacity-100 transition-opacity" />
-                    </a>
-                    <h3 className="text-lg font-semibold mb-2 sm:mb-0">
-                      {experience.jobTitle}
-                    </h3>
-                  </div>
-                  <div className="text-sm text-muted-foreground dark:text-gray-400 w-full sm:w-auto sm:text-right">
-                    <div className="flex items-center sm:justify-end">
-                      <span>
-                        {formatDateRange(experience.duration, {
-                          format: "month-year",
-                        })}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <ul className="list-disc list-inside mb-6 space-y-2">
-                  {experience.responsibilities.map((responsibility, i) => (
-                    <li
-                      key={i}
-                      className="text-sm leading-relaxed dark:text-gray-300"
-                    >
-                      {responsibility}
-                    </li>
-                  ))}
-                </ul>
-                <div className="flex flex-wrap gap-3">
-                  {experience.technologiesUsed.map(tech => (
-                    <TechnologyTag
-                      key={tech.name}
-                      name={tech.name}
-                      icon={tech.icon}
-                      color={tech.color}
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
+              experience={experience}
+              isLast={index === experiences.length - 1}
+            />
           ))}
         </div>
       </div>
