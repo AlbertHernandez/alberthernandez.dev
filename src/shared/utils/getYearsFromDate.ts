1ccf3a7d87e5169cdae002c57ef3622b4c595182
Albert Hernandez <alberthernandezdev@gmail.com>
2024-09-01 13:03:02 +0200

    refactor: remove dummy page

--- src/shared/utils/getYearsFromDate.ts
+++ src/shared/utils/getYearsFromDate.ts
@@ -1,11 +1,18 @@
-export const getYearsFromDate = (date: Date): number => {
-  const now = new Date();
+export const getYearsFromDate = (
+  date: Date,
+  options: { mockDate?: Date } = {},
+): number => {
+  const now = options.mockDate || new Date();
   let years = now.getFullYear() - date.getFullYear();
   const monthDifference = now.getMonth() - date.getMonth();
   const dayDifference = now.getDate() - date.getDate();
 
   if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
     years--;
+  }
+
+  if (years < 0) {
+    return 0;
   }
 
   return years;
--- tests/unit/app/dummy-page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
-
-import DummyPage from "@/app/dummy-page";
-
-test("DummyPage", () => {
-  render(<DummyPage />);
-  expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined();
-});
