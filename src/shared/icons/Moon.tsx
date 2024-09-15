48371966828a64ff21f5c6ff0bb84fc1c0187f21
Albert Hernandez <alberthernandezdev@gmail.com>
2024-09-15 19:28:09 +0200

    feat: remove unnecessary icons

--- src/shared/icons/Moon.tsx
+++
@@ -1,8 +0,0 @@
-import { Moon as LucideMoon } from "lucide-react";
-import React from "react";
-
-import { IconProps } from "./types";
-
-export const Moon: React.FC<IconProps> = ({ ...props }) => {
-  return <LucideMoon {...props} />;
-};
