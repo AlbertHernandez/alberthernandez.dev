7ee0cd76ad2ced4d4dd6bb70433acba74e3c4a73
Albert Hernandez <alberthernandezdev@gmail.com>
2024-09-15 22:24:51 +0200

    feat: center icons with job

--- src/portfolio/components/experience/components/experience-card/ExperienceCard.tsx
+++ src/portfolio/components/experience/components/experience-card/ExperienceCard.tsx
@@ -22,65 +22,67 @@ export const ExperienceCard: React.FC<ExperienceCardProps> = ({
   const [isLinkFocused, setIsLinkFocused] = useState(false);
 
   return (
-    <div className="relative flex items-start">
+    <div className="relative flex">
       {!isLast && (
         <div
-          className="absolute top-0 left-6 h-[calc(100%+4rem)] w-px bg-gray-300 dark:bg-gray-700"
+          className="absolute top-[3.3rem] left-6 h-[calc(100%)] w-px bg-gray-300 dark:bg-gray-700"
           aria-hidden="true"
         />
       )}
-      <div className="flex-shrink-0 w-12 h-12 rounded-full border-4 border-white dark:border-gray-800 bg-white dark:bg-gray-800 z-10">
-        <Image
-          src={experience.company.logo}
-          alt={`${experience.company.name} logo`}
-          width={48}
-          height={48}
-          className="rounded-full"
-          loading="lazy"
-        />
+      <div className="flex-shrink-0 w-12 h-12 mt-1">
+        <div className="w-12 h-12 rounded-full border-4 border-white dark:border-gray-800 bg-white dark:bg-gray-800 z-10 overflow-hidden">
+          <Image
+            src={experience.company.logo}
+            alt={`${experience.company.name} logo`}
+            width={48}
+            height={48}
+            className="rounded-full"
+            loading="lazy"
+          />
+        </div>
       </div>
       <div className="ml-6 flex-grow">
-        <div className="flex flex-col sm:flex-row justify-between items-start mb-2">
-          <div>
-            <h3 className="text-xl font-bold mb-1 text-gray-100">
-              <Link
-                href={experience.company.website}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-flex items-center group hover:underline"
-                onFocus={() => setIsLinkFocused(true)}
-                onBlur={() => setIsLinkFocused(false)}
-                aria-label={`${experience.company.name} (opens in a new tab)`}
-              >
-                {experience.company.name}
-                <ExternalLink
-                  className={`ml-1 h-4 w-4 transition-opacity ${
-                    isLinkFocused || isLinkFocused
-                      ? "opacity-100"
-                      : "opacity-0 group-hover:opacity-100"
-                  }`}
-                  aria-hidden="true"
-                />
-              </Link>
-            </h3>
-            <p className="text-lg font-semibold mb-2 sm:mb-0 text-gray-100">
+        <div className="flex flex-col sm:flex-row justify-between items-start">
+          <div className="flex flex-col">
+            <div className="flex items-center">
+              <h3 className="text-xl font-bold text-gray-100">
+                <Link
+                  href={experience.company.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center group hover:underline"
+                  onFocus={() => setIsLinkFocused(true)}
+                  onBlur={() => setIsLinkFocused(false)}
+                  aria-label={`${experience.company.name} (opens in a new tab)`}
+                >
+                  {experience.company.name}
+                  <ExternalLink
+                    className={`ml-1 h-4 w-4 transition-opacity ${
+                      isLinkFocused
+                        ? "opacity-100"
+                        : "opacity-0 group-hover:opacity-100"
+                    }`}
+                    aria-hidden="true"
+                  />
+                </Link>
+              </h3>
+            </div>
+            <p className="text-lg font-semibold text-gray-100 mt-1">
               {experience.jobTitle}
             </p>
           </div>
-          <div className="text-muted-foreground dark:text-gray-400 w-full sm:w-auto sm:text-right">
-            <div className="flex items-center sm:justify-end">
-              <time
-                dateTime={`${experience.duration.start.getFullYear()}-${experience.duration.end?.getFullYear() || "present"}`}
-              >
-                {formatDateRange(experience.duration, {
-                  format: "month-year",
-                })}
-              </time>
-            </div>
+          <div className="text-muted-foreground dark:text-gray-400 w-full sm:w-auto sm:text-right mt-2 sm:mt-0">
+            <time
+              dateTime={`${experience.duration.start.getFullYear()}-${experience.duration.end?.getFullYear() || "present"}`}
+            >
+              {formatDateRange(experience.duration, {
+                format: "month-year",
+              })}
+            </time>
           </div>
         </div>
         <ul
-          className="list-disc list-inside mb-6 space-y-2"
+          className="list-disc list-inside mt-4 mb-6 space-y-2"
           aria-label="Responsibilities"
         >
           {experience.responsibilities.map((responsibility, i) => (
